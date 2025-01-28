c71e11996296c995e15d2c05b5a4705162a67e87
Mix <pison.mick1@gmail.com>
2025-01-28 14:37:40 +0700

    lab15_4.cpp_completed

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -18,4 +18,16 @@ int main(){
 	return 0;
 }
 
+void shuffle(int &a,int &b,int &c,int &d){
+	int arr[] = {a,b,c,d};
+	for(int i=0; i<2; i++){
+		int x = rand()%4;
+		int y = rand()%4;
+		swap(arr[x],arr[y]);
+	}
+	a = arr[0];
+	b = arr[1];
+	c = arr[2];
+	d = arr[3];
+}
 //Do not modify source code above this line
